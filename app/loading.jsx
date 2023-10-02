8aa275f88b18f55707588e6cbdd7d0edb8e0cbbe
Husam Asaad <101347296+husamasaad@users.noreply.github.com>
2023-10-02 14:01:22 +0200

    added spinner to loading page

--- app/loading.jsx
+++ app/loading.jsx
@@ -1,13 +1,22 @@
 'use client'
 import React from 'react'
-import { Router } from 'next/navigation'
-import NProgress from 'nprogress'
+
+import { Oval } from 'react-loader-spinner'
 
 const loading = () => {
 
   return (
     <main className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 bg-primary relative">
-
+      <Oval
+        height="80"
+        width="80"
+        color="white"
+        ariaLabel="circles-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+        secondaryColor="#7676B2"
+      />
       
     </main>
 
