1a830adfd89ace22a0358bb1b7c18cb56722c61b
Husam Asaad <101347296+husamasaad@users.noreply.github.com>
2023-10-02 14:43:15 +0200

    adjusted sizes

--- components/FlexSection.jsx
+++ components/FlexSection.jsx
@@ -16,7 +16,7 @@ const FlexSection = ({ section }) => {
         <div className={`container mx-auto px-10 lg:px-20 flex items-center flex-wrap md:flex-nowrap ${flexDir} justify-center md:justify-between gap-10`}>
           <Image src={imgUrl} alt="" width={585} className='drop-shadow-sm rounded-md' />
           <div className="flex md:basis-[40%] flex-col gap-4">
-            <h2 className='text-5xl lg:text-6xl font-bold leading-snug text-primary m-0'>{heading}</h2>
+            <h2 className='text-3xl sm:text-5xl lg:text-6xl font-bold leading-snug text-primary m-0'>{heading}</h2>
             <p className='text-lg text-[rgba(35,35,64,0.90)] font-normal max-w-lg mt-4 mb-10'>{details}</p>
             <Link href="/" className='group flex items-center gap-1 underline text-blue-800 text-md'>Learn More <span className='group-hover:translate-x-1 transition-transform'><BsArrowRight /></span></Link>
           </div>
